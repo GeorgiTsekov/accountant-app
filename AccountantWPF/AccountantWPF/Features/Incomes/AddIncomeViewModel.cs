a6a970da4043d7c1900857ad762d8c807df74941
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-10-26 17:21:43 +0300

    AddIncomeView/Model, IncomeView/Model and MainWindow updated

--- AccountantWPF/AccountantWPF/Features/Incomes/AddIncomeViewModel.cs
+++ AccountantWPF/AccountantWPF/Features/Incomes/AddIncomeViewModel.cs
@@ -1,57 +1,187 @@
-﻿using AccountantWPF.Data.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+using AccountantWPF.Data.Models;
+using AccountantWPF.Features.CashPosIncomes;
+using AccountantWPF.Features.CashRegisters;
+using AccountantWPF.Features.Shifts;
 
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-
-using MaterialDesignThemes.Wpf;
 
 namespace AccountantWPF.Features.Incomes
 {
     public partial class AddIncomeViewModel : ObservableObject
     {
         private readonly IncomeService _incomeService;
+        private readonly CashPosService _cashPosService;
+        private readonly CashRegisterService _cashRegisterService;
+        private readonly ShiftService _shiftService;
+
         //private readonly ISnackbarMessageQueue _messageQueue;
 
         [ObservableProperty]
         [NotifyCanExecuteChangedFor(nameof(SubmitCommand))]
-        private string? _name;
+        private string? _incomeName;
 
         [ObservableProperty]
         [NotifyCanExecuteChangedFor(nameof(SubmitCommand))]
         private DateTime? _createdOn;
-        public AddIncomeViewModel(IncomeService incomeService, ISnackbarMessageQueue messageQueue)
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SubmitCommand))]
+        private string? _cashPosName;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SubmitCommand))]
+        private string? _cashRegisterName;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SubmitCommand))]
+        private string? _shiftName;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SubmitCommand))]
+        private string? _cashierName;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SubmitCommand))]
+        private int? _bonnets;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SubmitCommand))]
+        [Column(TypeName = "decimal(18,2)")]
+        private decimal? _shiftCash;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SubmitCommand))]
+        [Column(TypeName = "decimal(18,2)")]
+        private decimal? _shiftPos;
+
+        public AddIncomeViewModel(
+            IncomeService incomeService,
+            CashPosService cashPosService,
+            CashRegisterService cashRegisterService,
+            ShiftService shiftService)
         {
             _incomeService = incomeService;
+            _cashPosService = cashPosService;
+            _cashRegisterService = cashRegisterService;
+            _shiftService = shiftService;
             //_messageQueue = messageQueue ?? throw new ArgumentNullException(nameof(messageQueue));
         }
 
         [RelayCommand(CanExecute = nameof(CanSubmit))]
         private async Task OnSubmit()
         {
-            if (string.IsNullOrWhiteSpace(Name) || !CreatedOn.HasValue)
+            if (string.IsNullOrWhiteSpace(IncomeName) ||
+                string.IsNullOrWhiteSpace(CashPosName) ||
+                string.IsNullOrWhiteSpace(CashRegisterName) ||
+                string.IsNullOrWhiteSpace(ShiftName) ||
+                !CreatedOn.HasValue ||
+                !ShiftPos.HasValue ||
+                !ShiftCash.HasValue)
             {
                 return;
             }
-            var newIncome = new Income
-            {
-                CreatedOn = CreatedOn.Value.Date,
-                Name = Name
-            };
 
-            var created = await _incomeService.CreateAsync(newIncome);
+            var income = await _incomeService.ByNameAndDateAsync(IncomeName, CreatedOn.Value);
 
-            if (created)
+            if (income == null)
             {
-                Name = string.Empty;
-                CreatedOn = null;
-                //_messageQueue.Enqueue("Saved!");
+                income = new Income
+                {
+                    CreatedOn = CreatedOn.Value.Date,
+                    Name = IncomeName
+                };
+
+                await _incomeService.CreateAsync(income);
+            }
+
+            var cashPos = await _cashPosService.ByNameAndDateAsync(CashPosName, CreatedOn.Value);
+
+            if (cashPos == null)
+            {
+                cashPos = new CashPos
+                {
+                    CreatedOn = CreatedOn.Value.Date,
+                    Name = CashPosName,
+                    IncomeId = income.Id
+                };
+
+                await _cashPosService.CreateAsync(cashPos);
             }
             else
             {
-                //_messageQueue.Enqueue("Incorect Format!");
+                //_messageQueue.Enqueue("There is a asdas with same name and date!");
             }
+
+            var cashRegister = await _cashRegisterService.ByNameAndDateAsync(CashRegisterName, CreatedOn.Value);
+
+            if (cashRegister == null)
+            {
+                cashRegister = new CashRegister
+                {
+                    CreatedOn = CreatedOn.Value.Date,
+                    Name = CashRegisterName,
+                    CashPosId = cashPos.Id
+                };
+
+                await _cashRegisterService.CreateAsync(cashRegister);
+            }
+            else
+            {
+                //_messageQueue.Enqueue("There is a asdas with same name and date!");
+            }
+
+            var shift = await _shiftService.ByNameAndDateAsync(ShiftName, CreatedOn.Value);
+
+            if (shift == null)
+            {
+                shift = new Shift
+                {
+                    CreatedOn = CreatedOn.Value.Date,
+                    Name = ShiftName,
+                    CashierName = CashierName,
+                    Bonnets = Bonnets,
+                    Pos = ShiftPos.Value,
+                    Cash = ShiftCash.Value,
+                    CashRegisterId = cashRegister.Id
+                };
+
+                var created = await _shiftService.CreateAsync(shift);
+
+                if (created)
+                {
+                    //_messageQueue.Enqueue("Saved!");
+                }
+                else
+                {
+                    //_messageQueue.Enqueue("Incorect Format!");
+                }
+            }
+            else
+            {
+                //_messageQueue.Enqueue("There is a shift with same name and date!");
+            }
+
+            IncomeName = string.Empty;
+            CashPosName = string.Empty;
+            CashRegisterName = string.Empty;
+            ShiftName = string.Empty;
+            CashierName = string.Empty;
+            Bonnets = null;
+            CreatedOn = null;
+            ShiftPos = null;
+            ShiftCash = null;
         }
 
-        private bool CanSubmit() => !string.IsNullOrWhiteSpace(Name) && CreatedOn.HasValue;
+        private bool CanSubmit()
+            => !string.IsNullOrWhiteSpace(IncomeName) &&
+            !string.IsNullOrWhiteSpace(ShiftName) &&
+            !string.IsNullOrWhiteSpace(CashRegisterName) &&
+            !string.IsNullOrWhiteSpace(CashPosName) &&
+                CreatedOn.HasValue &&
+                ShiftPos.HasValue &&
+                ShiftCash.HasValue;
     }
 }
--- AccountantWPF/AccountantWPF/Features/Incomes/IncomeViewModel.cs
+++ AccountantWPF/AccountantWPF/Features/Incomes/IncomeViewModel.cs
@@ -1,20 +1,35 @@
 ﻿using AccountantWPF.Data.Models;
 
+using CommunityToolkit.Mvvm.Input;
+
 namespace AccountantWPF.Features.Incomes
 {
-    public class IncomeViewModel
+    public partial class IncomeViewModel
     {
+        public int Id { get; }
         public string Name { get; }
         public decimal Pos { get; }
         public decimal Cash { get; }
         public DateTime CreatedOn { get; }
+        public ICollection<CashPos> CashPosIncomes { get; set; }
 
         public IncomeViewModel(Income income)
         {
+            Id = income.Id;
             Name = income.Name;
             Pos = income.Pos;
             Cash = income.Cash;
             CreatedOn = income.CreatedOn;
+            CashPosIncomes = income.CashPosIncomes;
+        }
+
+        [RelayCommand]
+        public void OnAllAsync()
+        {
+            if (CashPosIncomes.Count > 0)
+            {
+                //TODO:
+            }
         }
     }
 }
